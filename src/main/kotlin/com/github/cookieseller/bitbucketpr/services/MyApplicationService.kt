8e608bed002dd3e10822093046eae7933adff924
GitHub Action <action@github.com>
2020-07-01 21:37:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/cookieseller/bitbucketpr/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cookieseller/bitbucketpr/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cookieseller.bitbucketpr.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cookieseller.bitbucketpr.MyBundle
 
 class MyApplicationService {
 
